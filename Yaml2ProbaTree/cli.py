aaece38f9d00d657101d112f5f8cdd2d1753846d
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2020-11-20 16:59:58 +0100

    Add latex refactoring feature

--- Yaml2ProbaTree/cli.py
+++ Yaml2ProbaTree/cli.py
@@ -1,9 +1,38 @@
 import click
+import re
 from Yaml2ProbaTree.yaml2probatree import Yaml2ProbaTree
+
+
+def extract_part(content):
+    p1 = re.compile('%% Tree Start.*')
+    p2 = re.compile('%% Tree End.*')
+    m1 = p1.search(content)
+    m2 = p2.search(content)
+    if not m1 or not m2:
+        return []
+    studied_part = content[m1.span()[0]:m2.span()[1]]
+    if studied_part == []:
+        return []
+    next_part = extract_part(content[m2.span()[1]:])
+    result = [studied_part]
+    for i in next_part:
+        result.append(i)
+    return result
 
 @click.command()
 @click.option('-d', '--debug', is_flag=True)
-@click.argument('input_yaml', type=click.Path(), required=False)
-def cli(input_yaml, debug):
+@click.option('-l', '--refactor-latex', type=click.Path(), required=False)
+@click.option('-y', '--input-yaml', type=click.Path(), required=False)
+def cli(debug, refactor_latex=None, input_yaml=None):
     yaml2probatree = Yaml2ProbaTree(debug)
-    print(yaml2probatree.yaml2tikz(input_yaml_file=input_yaml))
+    if input_yaml:
+        print(yaml2probatree.yaml2tikz(input_yaml_file=input_yaml))
+    elif refactor_latex:
+        with open(refactor_latex, "r") as refactor_latex_file:
+            content = ''.join(refactor_latex_file.readlines())
+            tabs = extract_part(content)
+            for m in tabs:
+                m = '\n'.join(m.split('\n')[1:][::-1][1:][::-1])
+                replacement = yaml2probatree.yaml2tikz(yaml_text=m)+"\n"
+                content = content.replace(m, replacement)
+        print(content)
--- Yaml2ProbaTree/yaml2probatree.py
+++ Yaml2ProbaTree/yaml2probatree.py
@@ -7,6 +7,8 @@ class Yaml2ProbaTree:
         self.debug = debug
 
     def indent(self, text):
+        if not text:
+            return ""
         text = "\t"+text
         return text.replace("\n", "\n\t")
 
@@ -43,18 +45,24 @@ class Yaml2ProbaTree:
             text = """\\node[mytree] {}\n\t""" + self.indent(text).strip()+ """;\n"""
         return text
 
-    def yaml2tikz(self, input_yaml_file=None):
-        if input_yaml_file == None:
+    def yaml2tikz(self, input_yaml_file=None, yaml_text=None):
+        if yaml_text != None:
+            # Yaml doesn't work with tabs
+            text = yaml_text.replace('\t', '  ')
+            data = load(text, Loader=Loader)
+        elif input_yaml_file == None:
             text = ''.join([ line for line in sys.stdin])
             # Yaml doesn't work with tabs
             text = text.replace('\t', '  ')
             if self.debug:
                 print(f"[log] stdin : `{text}`")
             data = load(text, Loader=Loader)
-        else:
+        elif input_yaml_file:
             with open(input_yaml_file, "r") as input_tree:
                 # Yaml doesn't work with tabs
-                input_tree = input_tree.replace('\t', '  ')
+                input_tree = ''.join(input_tree.readline()).replace('\t', '  ')
+                if self.debug:
+                    print(f"[log] yaml : `{text}`")
                 data = load(input_tree, Loader=Loader)
         if not "root" in data.keys():
             print("[error] No root node, exiting...")
